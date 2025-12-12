eb3610e173ba4ba356e65ee645d0de2ae4e3b8b8
mohammademon02 <mohammademon0207@gmail.com>
2025-12-12 10:56:58 +0600

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+
+  return (
+    <div>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/kanban" />} />
+          <Route path="/kanban" element={<KanbanPage />} />
+          <Route path="/list-view" element={<ListViewPage />} />
+        </Routes>
+      </BrowserRouter> */}
+      <div>
+        kanban page
+      </div>
+    </div>
+  )
+}
+
+export default App
